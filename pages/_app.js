9b72d430dd4c2a185bd3396834d140848b140ee9
Maddox Edwards <edwardsbma33@gmail.com>
2024-01-13 15:02:11 -0500

    maybe added analytics support

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
 import { useState } from 'react'
+import { Analytics } from '@vercel/analytics/react'
 
 function App({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(false)
 
-  return <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
+  return (
+    <>
+    <Component {...pageProps} darkMode={darkMode} setDarkMode={setDarkMode} />
+    <Analytics />
+    </>
+  )
 }
 
 export default App
